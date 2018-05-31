37ba54c90e9ae490f00f82ca616e62dccfcfc437
masayukimihira <ft.lvlv.dcn@gamil.com>
2018-05-31 06:04:37 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,9 +47,11 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'status' => 'required|max:10', 
             'content' => 'required|max:10',
         ]);
         $task= new Task;
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
@@ -96,9 +98,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
          $this->validate($request, [
+             'status' => 'required|max:10',
             'content' => 'required|max:1o',
         ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
