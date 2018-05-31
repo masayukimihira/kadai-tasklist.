9c0384dd9b74d26287ee7dc57b2eda2959da9d42
masayukimihira <ft.lvlv.dcn@gamil.com>
2018-05-31 05:38:51 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,6 +46,9 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'content' => 'required|max:10',
+        ]);
         $task= new Task;
         $task->content = $request->content;
         $task->save();
@@ -92,6 +95,9 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+         $this->validate($request, [
+            'content' => 'required|max:1o',
+        ]);
         $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
